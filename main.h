4ab1061a410c44213ef5c46916cbacbd5b4beb90
William05Cardozo <3784@holbertonschool.com>
2021-11-07 10:01:51 -0800

    main-h

--- main.h
+++ main.h
@@ -6,6 +6,18 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ * struct print - name struct
+ * @commands: pointer to commands
+ * @f: pointer to function
+ */
+
+typedef struct print
+{
+	char *commands;
+	int (*f)(va_list);
+} print_f;
+
 int _write(char c);
 int _printf(const char *format, ...);
 #endif
