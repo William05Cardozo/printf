bd1e1ff1b9a5cfe383b6dd93d5a8e570cbca78fe
xARKEINx <3884@holbertonschool.com>
2021-11-07 20:00:44 -0800

    getcommands

---
+++ get_comants.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * get_commands - entry point
+ * i: variable
+ * @s: pointer
+ * Return: NULL
+ */
+int (*get_commands(const char *s))(va_list)
+{
+	print_f print[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{NULL, NULL},
+	};
+
+	int i;
+
+	for (i = 0; print[i].commands != NULL; i++)
+	{
+		if (*s == *print[i].commands)
+		{
+			return (print[i].f);
+		}
+	}
+	return (NULL);
+}
