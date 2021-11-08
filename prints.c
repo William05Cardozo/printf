b00b8164f6ecd589896e4b24d68c934367b759f0
William05Cardozo <3784@holbertonschool.com>
2021-11-08 10:25:40 -0800

    Prints

--- prints.c
+++ prints.c
@@ -1,7 +1,12 @@
 #include "main.h"
 /**
- *
+ * print_char - Function print a character
+ * print_string - Function print a string
+ * @list: contains the list of arguments
+ * a: iterator
+ * Return: a
  */
+
 int print_char(va_list list) /*funcion va_list guarda los argumentos dados*/
 {
 	char c = va_arg(list, int); /*guarda el entero*/
@@ -9,6 +14,7 @@ int print_char(va_list list) /*funcion va_list guarda los argumentos dados*/
 	_putchar(c); /*imprime el entero*/
 	return (1); /*devuelve un caracter*/
 }
+
 int print_string(va_list list) /*guarda las strings dadas*/
 {
 	int a; /*declaramos iterador*/
@@ -17,13 +23,13 @@ int print_string(va_list list) /*guarda las strings dadas*/
 	if (s == NULL) /*condicion de comparacion al NULL*/
 	{
 		s = "(null)"; /*retorna al NULL si se cumple condicion*/
-	}  
+	}
 
 	for (a = 0; s[a] != '\0'; a++) /*recorremos la string*/
 	{
 		_putchar (s[a]);
 	}
-	
+
 	return (a); /*imprime iterador*/
 
 }
