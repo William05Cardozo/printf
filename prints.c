880a6d7e4cbc5fc691a60da46d7bc6fd14729a22
William05Cardozo <3784@holbertonschool.com>
2021-11-08 10:02:34 -0800

    prints

--- prints.c
+++ prints.c
@@ -2,23 +2,28 @@
 /**
  *
  */
-int print_char(va_list list)//funcion va_list guarda los argumentos dados//
+int print_char(va_list list) /*funcion va_list guarda los argumentos dados*/
 {
-	char c = va_arg(list, int);//guarda el entero//
+	char c = va_arg(list, int); /*guarda el entero*/
 
-	_putchar(c);//imprime el entero//
-	return (1);//devuelve un caracter//
+	_putchar(c); /*imprime el entero*/
+	return (1); /*devuelve un caracter*/
 }
-int print_string(va_list list)//guarda las strings dadas//
+int print_string(va_list list) /*guarda las strings dadas*/
 {
-	int a;//declaramos iterador//
-	char *s = va_arg(list, char *);//se le asigna un argumento//
+	int a; /*declaramos iterador*/
+	char *s = va_arg(list, char *); /*se le asigna un argumento*/
 
-	for (a = 0; s[a] != '\0'; a++);//recorremos la string//
-	
-	if (s == NULL)//condicion de comparacion al NULL//
+	if (s == NULL) /*condicion de comparacion al NULL*/
 	{
-		return (NULL);//retorna al NULL si se cumple condicion//
+		s = "(null)"; /*retorna al NULL si se cumple condicion*/
+	}  
+
+	for (a = 0; s[a] != '\0'; a++) /*recorremos la string*/
+	{
+		_putchar (s[a]);
 	}
-	return (a);//imprime iterador//
+	
+	return (a); /*imprime iterador*/
+
 }
