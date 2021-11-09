4bd908319a5c549b4a474e63e6b29b3e803202ec
William05Cardozo <3784@holbertonschool.com>
2021-11-09 09:09:25 -0800

    Correction

--- main.h
+++ main.h
@@ -24,11 +24,8 @@ void print_all(int n);
 int print_char(va_list list);
 int print_string(va_list list);
 int print_int(va_list list);
-<<<<<<< HEAD
 int print_dex(va_list);
 int print_pc(va_list __attribute__((unused)) list);
-=======
 int print_dex(va_list list);
->>>>>>> 2a38d277ab4c4e9e3066de53f7ac2c52e3ed419d
 int (*get_commands(const char *s))(va_list);
 #endif
