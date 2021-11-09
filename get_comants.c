9bded01d8f77404239ede27b503284e0c6235d6a
xARKEINx <3884@holbertonschool.com>
2021-11-09 09:18:35 -0800

    all

--- get_comants.c
+++ get_comants.c
@@ -10,12 +10,9 @@ int (*get_commands(const char *s))(va_list)
 	print_f print[] = {
 		{"c", print_char},
 		{"s", print_string},
-<<<<<<< HEAD
 		{"%", print_pc},
-=======
 		{"i", print_dex},
 		{"d", print_int},
->>>>>>> 2a38d277ab4c4e9e3066de53f7ac2c52e3ed419d
 		{NULL, NULL},
 	};
 
