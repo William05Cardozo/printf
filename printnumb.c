1c647bd715d9a92ca5738a4ca61427ae58e66046
xARKEINx <3884@holbertonschool.com>
2021-11-09 19:21:28 -0800

    clear

--- printnumb.c
+++ printnumb.c
@@ -42,16 +42,3 @@ int print_int(va_list list)
 	}
 	return (cont);
 }
-/**
-* print_dex - Print decimal numbers
-* @list: Parameters
-* i: Variable
-* Return: 1 no errors
-*/
-int print_dex(va_list list)
-{
-	int i = va_arg(list, int);
-
-	print_all(i);
-	return (1);
-}
